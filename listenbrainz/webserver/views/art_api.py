c631d5c9b850c48337b868b8a767a1d6af82214b
Monkey Do <salicon.petiller@gmail.com>
2025-12-12 12:41:28 +0100

    YIM25: Fix playlist SVG links and caption

--- listenbrainz/webserver/views/art_api.py
+++ listenbrainz/webserver/views/art_api.py
@@ -424,7 +424,7 @@ def _cover_art_yim_artists(user_name, stats, year, styles):
     )
 
 
-def _cover_art_yim_playlist(user_name, stats, key, year, branding, styles):
+def _cover_art_yim_playlist(user_name, stats, key, year, branding, show_caption, styles):
     """ Create the SVG using playlist tracks' cover arts for the given YIM playlist. """
     if stats.get(key) is None:
         return None
@@ -439,9 +439,17 @@ def _cover_art_yim_playlist(user_name, stats, key, year, branding, styles):
     for track in stats[key]["track"]:
         additional_metadata = track["extension"][PLAYLIST_TRACK_EXTENSION_URI].get("additional_metadata")
         if additional_metadata.get("caa_id") and additional_metadata.get("caa_release_mbid"):
-            caa_id = additional_metadata["caa_id"]
+            caa_id = additional_metadata.get("caa_id")
+            caa_release_mbid = additional_metadata.get("caa_release_mbid")
             # check existence in set to avoid duplicates
             if caa_id not in selected_urls:
+                images.append({
+                    "caa_id": caa_id,
+                    "caa_release_mbid": caa_release_mbid,
+                    "title": track.get("title"),
+                    "entity_mbid": str(track.get("mbid")),
+                    "artist": track.get("artist_credit")
+                })
                 images.append(additional_metadata)
                 selected_urls.add(caa_id)
     
@@ -464,7 +472,7 @@ def _cover_art_yim_playlist(user_name, stats, key, year, branding, styles):
             entity="album",
             width=500,
             height=500,
-            show_caption=False
+            show_caption=show_caption
         )
 
     match key:
@@ -483,6 +491,7 @@ def _cover_art_yim_playlist(user_name, stats, key, year, branding, styles):
         branding=branding,
         width=924,
         height=924,
+        show_caption=show_caption,
         **styles,
     )
 
@@ -556,6 +565,7 @@ def cover_art_yim(user_name, year: int = 2025):
             "Type of Image needs to be specified should be one of (overview, stats, artists, albums, tracks, discovery-playlist, missed-playlist)")
 
     branding = _parse_bool_arg("branding", True)
+    show_caption = _parse_bool_arg("show_caption", True)
 
     stats = db_yim.get(user["id"], year)
     if stats is None:
@@ -573,8 +583,8 @@ def cover_art_yim(user_name, year: int = 2025):
         case "albums": svg = _cover_art_yim_albums(user_name, stats, year, styles)
         case "tracks": svg = _cover_art_yim_tracks(user_name, stats, year, styles)
         case "artists": svg = _cover_art_yim_artists(user_name, stats, year, styles)
-        case "discovery-playlist": svg = _cover_art_yim_playlist(user_name, stats, "playlist-top-discoveries-for-year", year, branding, styles)
-        case "missed-playlist": svg = _cover_art_yim_playlist(user_name, stats, "playlist-top-missed-recordings-for-year", year, branding, styles)
+        case "discovery-playlist": svg = _cover_art_yim_playlist(user_name, stats, "playlist-top-discoveries-for-year", year, branding, show_caption, styles)
+        case "missed-playlist": svg = _cover_art_yim_playlist(user_name, stats, "playlist-top-missed-recordings-for-year", year, branding, show_caption, styles)
         case other: raise APIBadRequest(f"Invalid image type {other}. Image type should be one of (stats, artists, albums, tracks, discovery-playlist, missed-playlist)")
 
     if svg is None:
